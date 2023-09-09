81de96331ce87a4a667b18cf5d7f7030b6e1906d
Rafiul-Islam <rafi.cse.ru@gmail.com>
2023-09-09 14:12:14 +0600

    navbar component done

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var myNav = document.getElementById("navbar");
+
+window.onscroll = function () {
+  "use strict";
+  if (
+    document.body.scrollTop >= 70 ||
+    document.documentElement.scrollTop >= 70
+  ) {
+    myNav.classList.add("scroll");
+  } else {
+    myNav.classList.remove("scroll");
+  }
+};
